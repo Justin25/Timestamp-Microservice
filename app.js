e2ed7f4d7a403c92c2b94cfd086620a52b37b741
Justin25 <justinagruber@gmail.com>
2017-08-13 10:06:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Basic required imports
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+//Create instance of express
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, function(){
+    console.log('Working');
+})
