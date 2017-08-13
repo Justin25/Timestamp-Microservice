9b3a8ba31e30bcaeaad91ffc8d48a2859923d01e
Justin25 <justinagruber@gmail.com>
2017-08-13 11:21:22 -0400

    Initial working build

--- app.js
+++ app.js
@@ -6,7 +6,31 @@ var cors = require('cors');
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
-
+//Return json formatted for date
+app.get('/dateValues/:dateVal', function(req,res,next){
+    //Gets the date input
+    var dateVal = req.params.dateVal;
+    var dateFormat = ('ll');
+    var dateFormattingOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    if (isNaN(dateVal)) {
+        var naturalDate = new Date(dateVal);
+        naturalDate = naturalDate.toLocaleDateString("en-us",dateFormattingOptions);
+        var unixDate = new Date(dateVal).getTime()/1000;
+    }
+    else {
+        var unixDate = dateVal;
+        var naturalDate = new Date(dateVal * 1000)
+        naturalDate = naturalDate.toLocaleDateString("en-us",dateFormattingOptions);
+    }
+    if (isNaN(unixDate)) {
+        naturalDate = null;
+    }
+    res.json({unix: unixDate, natural: naturalDate});
+});
 app.listen(3000, function(){
-    console.log('Workingg');
-})
+    console.log('Working');
+});
